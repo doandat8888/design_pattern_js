8b7cbf40ab22b86451a4dc802894316961658bb4
unknown <badatdoan@gmail.com>
2024-05-21 09:54:46 +0700

    add factory pattern

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log('This is design pattern js');
 
 // Singleton pattern
 console.log('This is singleton pattern');
+
+// Factory pattern
+console.log('This is factory pattern');
+
+// Strategy pattern
+console.log('This is strategy pattern');
